4f764ce3b989d395fdb30e756e712140e8900aec
Felipe Ospina <pipe.98.oh@gmail.com>
2018-08-13 20:00:59 -0500

    Creating the DB beans and conections
    Controllers, models, beans, pools, and others created to AwessomeCRUD DB, and optimizing template and login screen.

---
+++ build/generated-sources/ap-source-output/model/Users_.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-13T19:55:48")
+@StaticMetamodel(Users.class)
+public class Users_ { 
+
+    public static volatile SingularAttribute<Users, Date> birthday;
+    public static volatile SingularAttribute<Users, String> img;
+    public static volatile SingularAttribute<Users, String> pass;
+    public static volatile SingularAttribute<Users, String> name;
+    public static volatile SingularAttribute<Users, String> email;
+    public static volatile SingularAttribute<Users, String> lastname;
+
+}
